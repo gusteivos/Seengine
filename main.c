3fbf70ced3dc55a1ff20c28c49a8fe33f26d8a6f
Gusteivos gp <gc5142387@gmail.com>
2023-01-22 21:19:38 -0300

    reboot

--- main.c
+++ main.c
@@ -1,33 +1,8 @@
-#include <service/service.h>
+#include <stdio.h>
 
-int main(int _argument_count, char *_argument_values[])
+int main(int argument_count, char *argument_values[])
 {
 
-    if (!startedForService(_argument_count, _argument_values))
-    {
 
-        /*
-
-            TODO:
-
-        */
-
-    }
-    #ifndef _WIN32 /*POSIX */
-
-        else
-        {
-
-            char *__argument_values_to_service_entry[0];
-
-            int __argument_count_to_service_entry = prepareArgumentsForPosixServiceEntry(_argument_count, _argument_values, __argument_values_to_service_entry);
-
-            posixServiceEntry(__argument_count_to_service_entry, __argument_values_to_service_entry);
-
-        }
-
-    #endif
-
-    return exit_code_for_service_executable;
 
 }
